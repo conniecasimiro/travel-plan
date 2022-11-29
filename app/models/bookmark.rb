6aa2ecefc22bdf53eda6fe8be30edba2b459856e
harry fleminger <harry.fleminger@hotmail.com>
2022-11-29 13:03:44 +0000

    Added db associations and vadliations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,5 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :trip
+  validates :user_id, uniqueness: { scope: :trip_id, message: "You've already favourited this trip " }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :trip
+  validates :description, presence: true
 end
