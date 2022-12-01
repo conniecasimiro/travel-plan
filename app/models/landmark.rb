accbcdc0e4b40bad08a9024f40abfa470cddd36a
Constanca Casimiro <duarte.connie2003@gmail.com>
2022-12-01 22:22:58 +0000

    map routes done

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -6,5 +6,5 @@ class Landmark < ApplicationRecord
   # validates :title, :description, uniqueness: true
   # validates :photo, attached: true
   geocoded_by :location
-  after_validation :geocode
+  after_validation :geocode, if: :will_save_change_to_location?
 end
