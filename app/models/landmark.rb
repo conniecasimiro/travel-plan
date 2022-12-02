d9d341f0a643dd012a8729e1e16e57500b40ca97
harry fleminger <harry.fleminger@hotmail.com>
2022-12-02 13:35:05 +0000

    It looks fugly, but it works, will work on details next

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -2,8 +2,8 @@ class Landmark < ApplicationRecord
   has_one_attached :photo
   belongs_to :route
   has_one :trip, through: :route
-  # validates :title, :location, :description, presence: true
-  # validates :title, :description, uniqueness: true
+  validates :title, :location, :description, presence: true
+  validates :title, :description, uniqueness: true
   # validates :photo, attached: true
   geocoded_by :location
   after_validation :geocode
