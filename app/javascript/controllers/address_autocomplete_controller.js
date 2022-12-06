8fa8b50ba9179e031f1591c6498a5ab056eba9a6
harry fleminger <harry.fleminger@hotmail.com>
2022-12-06 17:21:37 +0000

    Finished adding mapbox auto complete to the add route part of the overall create process

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -6,15 +6,30 @@ export default class extends Controller {
   static values = { apiKey: String }
   static targets = ["address", "search"]
 
-
+  connect() {
+    console.log("connected!")
+  }
   revealMap() {
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address"
     })
     this.geocoder.addTo(this.searchTarget)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+
   }
   disconnect() {
     this.geocoder.onRemove()
+  }
+
+  #setInputValue(event) {
+    console.log("setting destination value")
+    console.log(event)
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
   }
 }
